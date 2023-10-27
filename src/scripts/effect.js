ce26c16c782694b66d33640347886e48061f0925
luongnguyeen <147536458+luongnguyeen@users.noreply.github.com>
2023-10-28 01:40:13 +0900

    Update effect.js

--- src/scripts/effect.js
+++ src/scripts/effect.js
@@ -147,7 +147,7 @@ $('document').ready(function(){
 		$('#self').animate({top:mid-mid_diff*4,left:vw},3000);
 
 		$('#f1').css({"display": "block", "top":500*Math.random()+'px',"left":'0px'});
-		$('#f1').animate({top:mid-mid_diff*3,left:vw-vw_diff*6},3000);
+		$('#f1').animate({top:mid-mid_diff*3,left:vw-vw_diff*15},3000);
 
 
 		$('#f2').css({"display": "block", "top":500*Math.random()+'px',"left":'0px'});
